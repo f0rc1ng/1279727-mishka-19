13cf482c4e2cfbc8fc69cdd72532c32fef98f599
f0rc1ng <forcenig_91@mail.ru>
2020-05-23 16:12:12 +0300

    Подготовка к защите
    Первая попытка

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 "use strict";
 
 var gulp = require("gulp");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var webp = require("gulp-webp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+var del = require("del");
+var svgstore = require("gulp-svgstore");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp
@@ -15,14 +23,16 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -30,7 +40,73 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("images", function () {
+  return gulp
+    .src("source/img/*.{png,jpg,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.mozjpeg({ progressive: true }),
+        imagemin.svgo(),
+      ])
+    )
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp
+    .src("source/img/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp
+    .src("source/img/*.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp
+    .src(
+      [
+        "source/fonts/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/*.ico",
+      ],
+      {
+        base: "source",
+      }
+    )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -2,25 +2,45 @@ var navMain = document.querySelector(".site-list");
 var navToggle = document.querySelector(".main-nav__toggle");
 var navOff = document.querySelector(".user-list");
 
-navMain.classList.remove("main-nav--nojs");
-navOff.classList.remove("main-nav--nojs");
+navMain.classList.remove("main-nav__nojs");
+navOff.classList.remove("main-nav__nojs");
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
+  if (navMain.classList.contains("main-nav__closed")) {
+    navMain.classList.remove("main-nav__closed");
+    navMain.classList.add("main-nav__opened");
   } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav__closed");
+    navMain.classList.remove("main-nav__opened");
   }
 });
 
 navToggle.addEventListener("click", function () {
-  if (navOff.classList.contains("main-nav--closed")) {
-    navOff.classList.remove("main-nav--closed");
-    navOff.classList.add("main-nav--opened");
+  if (navOff.classList.contains("main-nav__closed")) {
+    navOff.classList.remove("main-nav__closed");
+    navOff.classList.add("main-nav__opened");
   } else {
-    navOff.classList.add("main-nav--closed");
-    navOff.classList.remove("main-nav--opened");
+    navOff.classList.add("main-nav__closed");
+    navOff.classList.remove("main-nav__opened");
+  }
+});
+
+var modal = document.querySelector(".popup-cart");
+var modal_wrapper = document.querySelector(".popup-cart__wrapper");
+var modal_btn = document.querySelector(".popup-cart__button");
+
+var btn = document.querySelector(".product-week__button");
+
+var openPopUp = function () {
+  modal.classList.remove("popup-cart__closed");
+};
+
+var closePopUp = function () {
+  modal.classList.add("popup-cart__closed");
+};
+
+modal.addEventListener("click", function (e) {
+  if (!modal_wrapper.contains(e.target)) {
+    closePopUp();
   }
 });
