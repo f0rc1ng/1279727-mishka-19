257654c8e8b0170e8db56acffeffec70507250d1
f0rc1ng <forcenig_91@mail.ru>
2020-05-25 01:32:30 +0300

    Подготовка к защите
    Последняя попытка

--- source/js/script.js
+++ source/js/script.js
@@ -2,26 +2,26 @@ var navMain = document.querySelector(".site-list");
 var navToggle = document.querySelector(".main-nav__toggle");
 var navOff = document.querySelector(".user-list");
 
-navMain.classList.remove("main-nav__nojs");
-navOff.classList.remove("main-nav__nojs");
+navMain.classList.remove("main-nav__list--nojs");
+navOff.classList.remove("main-nav__list--nojs");
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav__closed")) {
-    navMain.classList.remove("main-nav__closed");
-    navMain.classList.add("main-nav__opened");
+  if (navMain.classList.contains("main-nav__list--closed")) {
+    navMain.classList.remove("main-nav__list--closed");
+    navMain.classList.add("main-nav__list--opened");
   } else {
-    navMain.classList.add("main-nav__closed");
-    navMain.classList.remove("main-nav__opened");
+    navMain.classList.add("main-nav__list--closed");
+    navMain.classList.remove("main-nav__list--opened");
   }
 });
 
 navToggle.addEventListener("click", function () {
-  if (navOff.classList.contains("main-nav__closed")) {
-    navOff.classList.remove("main-nav__closed");
-    navOff.classList.add("main-nav__opened");
+  if (navOff.classList.contains("main-nav__list--closed")) {
+    navOff.classList.remove("main-nav__list--closed");
+    navOff.classList.add("main-nav__list--opened");
   } else {
-    navOff.classList.add("main-nav__closed");
-    navOff.classList.remove("main-nav__opened");
+    navOff.classList.add("main-nav__list--closed");
+    navOff.classList.remove("main-nav__list--opened");
   }
 });
 
@@ -32,11 +32,11 @@ var modal_btn = document.querySelector(".popup-cart__button");
 var btn = document.querySelector(".product-week__button");
 
 var openPopUp = function () {
-  modal.classList.remove("popup-cart__closed");
+  modal.classList.remove("popup-cart--closed");
 };
 
 var closePopUp = function () {
-  modal.classList.add("popup-cart__closed");
+  modal.classList.add("popup-cart--closed");
 };
 
 modal.addEventListener("click", function (e) {
